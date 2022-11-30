74c81aab0f8a07bd1370b7cfd0614fdb2d98207f
Victor Guaraná <victorguarana@hotmail.com>
2022-11-30 13:59:29 -0300

    show unpaid transactions

--- lib/components/transaction_widget.dart
+++ lib/components/transaction_widget.dart
@@ -10,24 +10,38 @@ class TransactionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(transaction.name),
-              RealText(value: transaction.value, changeColor: true),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                transaction.account,
-                style: Theme.of(context).textTheme.bodySmall,
+          Row(children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Container(
+                height: 5,
+                width: 5,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(5)),
+                  color: transaction.alreadyPaid ? null : Colors.orange,
+                ),
               ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(transaction.name),
+                Text(
+                  transaction.account,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+          ]),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              RealText(value: transaction.value, changeColor: true),
               Text(
-                transaction.createdWhenFormatted(),
+                transaction.payDayFormatted(),
                 style: Theme.of(context).textTheme.bodySmall,
               ),
             ],
